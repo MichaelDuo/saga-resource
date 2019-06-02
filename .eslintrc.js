018e6f8ceacbb0c7f9db0c24dd2b1a2a1b3926f7
MichaelDuo <michaeldong21@gmail.com>
2019-06-02 22:55:55 +0800

    Update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-	parser: "@typescript-eslint/parser",
+	parser: '@typescript-eslint/parser',
 	extends: [
-		"plugin:@typescript-eslint/recommended",
-		"plugin:prettier/recommended",
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
 	],
-	plugins: ["@typescript-eslint"],
+	plugins: ['@typescript-eslint'],
 	rules: {
-		"@typescript-eslint/indent": 0, // confilict with prettier
+		'@typescript-eslint/indent': 0, // confilict with prettier
+		'@typescript-eslint/no-explicit-any': 0,
 	},
 };
