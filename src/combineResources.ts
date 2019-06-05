a19470c6ff2df7d9ba80384b0a6097148911961b
MichaelDuo <michaeldong21@gmail.com>
2019-06-05 14:54:10 +0800

    update installation methods

--- src/combineResources.ts
+++ src/combineResources.ts
@@ -1,6 +1,7 @@
-import {Reducer, Action} from 'redux';
+import {Reducer, Action, combineReducers} from 'redux';
 import SagaResource, {ResourceState} from './SagaResource';
 import {mapValues, values} from 'lodash';
+import {all, Effect} from 'redux-saga/effects';
 
 type ReducersMapObject<S = any, A extends Action = Action> = {
 	[K in keyof S]: Reducer<ResourceState<S[K]>, A>
@@ -9,22 +10,44 @@ type ReducersMapObject<S = any, A extends Action = Action> = {
 type ResourceMapObject<S> = {[K in keyof S]: SagaResource<S[K]>};
 
 interface CombinedReducersAndSagas<S> {
-	reducers: ReducersMapObject<S>;
-	sagas: any[];
-	// makeReducer: <K>(reducer: Reducer<K>) => Reducer<K & ReducersMapObject<S>>;
+	getReducer: <K>(rootReducer?: Reducer<K>) => Reducer<K & S>;
+	getSaga: (saga: any) => any;
 }
 
 export default function combineResources<S>(
 	resources: ResourceMapObject<S>
 ): CombinedReducersAndSagas<S> {
+	const reducers = (mapValues(
+		resources,
+		(resource: any): any => resource.reducer
+	) as unknown) as ReducersMapObject<S>;
+
 	const result: CombinedReducersAndSagas<S> = {
-		reducers: (mapValues(
-			resources,
-			(resource: any): any => resource.reducer
-		) as unknown) as ReducersMapObject<S>,
-		sagas: values(resources).map(
-			(resource: any): any => resource.combinedSaga
-		) as any[],
+		getReducer: (rootReducer?: any): any => {
+			const combinedReducer = combineReducers(reducers as any);
+			return function(state: any, action: any): any {
+				if (rootReducer) {
+					return Object.assign(
+						rootReducer(state, action),
+						combinedReducer(state, action)
+					);
+				} else {
+					return combinedReducer(state, action);
+				}
+			};
+		},
+		getSaga: (rootSaga?: any): any => {
+			return function*(): IterableIterator<Effect> {
+				yield all([
+					rootSaga(),
+					...values(resources).map(
+						(resource: any): any => {
+							return resource.combinedSaga();
+						}
+					),
+				]);
+			};
+		},
 	};
 
 	return result;
